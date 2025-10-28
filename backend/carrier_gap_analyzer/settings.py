bbf30ffce7df5dcd10a4b1b298065d7eb4c26385
Mithilesh Maurya <mauryamithilesh1@gmail.com>
2025-10-28 14:15:15 +0530

    update set

--- backend/carrier_gap_analyzer/settings.py
+++ backend/carrier_gap_analyzer/settings.py
@@ -166,11 +166,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,"backend", 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,"backend", 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
