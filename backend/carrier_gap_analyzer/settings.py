675e4782590e5b30022dc6162415621c682b8c68
Mithilesh Maurya <mauryamithilesh1@gmail.com>
2025-10-28 14:23:30 +0530

    update set

--- backend/carrier_gap_analyzer/settings.py
+++ backend/carrier_gap_analyzer/settings.py
@@ -167,6 +167,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files
 MEDIA_URL = '/media/'
