786f95b31402a6fd14428520d01ff83ce8516c63
Mithilesh Maurya <mauryamithilesh1@gmail.com>
2025-11-01 19:36:38 +0530

    Update nlp_setup.py

--- backend/analysis/nlp_module/nlp_setup.py
+++ backend/analysis/nlp_module/nlp_setup.py
@@ -1,13 +1,14 @@
-import spacy 
+import spacy
 import subprocess
+import sys
 from spacy.cli import download
 
 def get_nlp():
     try:
-        nlp=spacy.load("en_core_web_sm")
+        nlp = spacy.load("en_core_web_sm")
     except OSError:
-        subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
-        nlp=spacy.load("en_core_web_sm")
+        subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"], check=True)
+        nlp = spacy.load("en_core_web_sm")
     return nlp
 
-nlp=get_nlp()
+nlp = get_nlp()
